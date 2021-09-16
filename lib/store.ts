06044d2f30692541624ee229cd841ab69b4d3176
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-09-16 13:52:23 +0200

    Migration to redux state management

--- lib/store.ts
+++ lib/store.ts
@@ -1,53 +1,14 @@
-import { types } from "mobx-state-tree";
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice';
+import uiLoginReducer from './uiLoginSlice';
 
-const UI = types
-  .model({
-    createForm: types.boolean,
-    loginForm: types.boolean,
-    deleteForm: types.boolean,
-  })
-  .actions((self) => ({
-    setNewBoard(status) {
-      self.createForm = status;
-    },
-    setLoginForm(status) {
-      self.loginForm = status;
-    },
-    setDeleteForm(status) {
-      self.deleteForm = status;
-    },
-  }));
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    uiLogin: uiLoginReducer
+  },
+})
 
-export const ui = UI.create({
-  createForm: false,
-  loginForm: false,
-  deleteForm: false,
-});
 
-const CANVAS = types
-  .model({
-    board: types.string,
-    title: types.string,
-    description: types.string,
-    user_id: types.string,
-  })
-  .actions((self) => ({
-    setBoard(status) {
-      self.board = status;
-    },
-    setTitle(status) {
-      self.title = status;
-    },
-    setDescription(status) {
-      self.description = status;
-    },
-    setUserId(status) {
-      self.user_id = status;
-    },
-  }));
-export const canvas = CANVAS.create({
-  board: JSON.stringify({ lines: [], width: "auto", height: 450 }),
-  title: "",
-  description: "",
-  user_id: "",
-});
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
 import { Auth } from "@supabase/ui";
 import dynamic from "next/dynamic";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { supabase } from "../lib/initSupabase";
 import "../styles/globals.css";
 import GlobalStyles from "./../components/GlobalStyles";
+import { Provider } from 'react-redux'
+import { store } from "lib/store";
 
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
   return (
-    <Auth.UserContextProvider supabaseClient={supabase}>
+    <Provider store={store}>
+    {/* <Auth.UserContextProvider supabaseClient={supabase}> */}
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
-    </Auth.UserContextProvider>
+    {/* </Auth.UserContextProvider> */}
+    </Provider>
   );
 }
 
