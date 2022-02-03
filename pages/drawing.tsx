ca697b503b772d1dd73d019a0282a44b95da9b56
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2022-02-03 18:14:02 +0100

    Download drawing as an image

--- pages/drawing.tsx
+++ pages/drawing.tsx
@@ -19,6 +19,7 @@ import {
   PencilIcon,
   FastForwardIcon,
   XCircleIcon,
+  DownloadIcon,
 } from "@heroicons/react/solid";
 import Head from "next/head";
 import React, {
@@ -27,6 +28,7 @@ import React, {
   useEffect,
   useState,
   useRef,
+  createElement,
 } from "react";
 import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
 import useWebSocket, { ReadyState } from "react-use-websocket";
@@ -93,6 +95,7 @@ const Drawing = () => {
   };
   const onUpdate = (updatedPaths: CanvasPath[]): void => {
     setPaths(updatedPaths);
+    console.log("s");
   };
 
   // Saving Drawing
@@ -149,7 +152,14 @@ const Drawing = () => {
       eraseMode(true);
     }
   };
-
+  const exportImage = () => {
+    canvasRef.current?.exportImage("jpeg").then((res) => {
+      let a = document.createElement("a");
+      a.href = res;
+      a.download = "Image.jpeg";
+      a.click();
+    });
+  };
   // Load unto the canvas
   const loadPath = (canvasPath: CanvasPath[]) => {
     const load = canvasRef.current?.loadPaths;
@@ -335,12 +345,12 @@ const Drawing = () => {
                 onChange={changeTitle}
               />
             </div>
-            <div className="flex mt-5 lg:mt-0 lg:ml-4">
-              <span className="">
+            <div className="flex flex-wrap justify-start mt-5 lg:mt-0 lg:ml-4">
+              <span className="ml-3">
                 <button
                   onClick={undoHandler}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <ReplyIcon
                     className="w-5 h-5 mr-2 -ml-1 text-gray-500"
@@ -354,7 +364,7 @@ const Drawing = () => {
                 <button
                   onClick={resetCanvasHandler}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <TrashIcon
                     className="w-5 h-5 mr-2 -ml-1 text-gray-500"
@@ -367,7 +377,7 @@ const Drawing = () => {
                 <button
                   onClick={redoHandler}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <FastForwardIcon
                     className="w-5 h-5 mr-2 -ml-1 text-gray-500"
@@ -380,7 +390,7 @@ const Drawing = () => {
                 <button
                   onClick={eraserHandler}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <XCircleIcon
                     className="w-5 h-5 mr-2 -ml-1 text-gray-500"
@@ -393,7 +403,7 @@ const Drawing = () => {
                 <button
                   onClick={penHandler}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <PencilIcon
                     className="w-5 h-5 mr-2 -ml-1 text-gray-500"
@@ -407,13 +417,26 @@ const Drawing = () => {
                 <button
                   onClick={onSubmit}
                   type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <SaveAsIcon
                     className="w-5 h-5 mr-2 -ml-1"
                     aria-hidden="true"
                   />
                   Save
+                </button>
+              </span>
+              <span className="ml-3">
+                <button
+                  onClick={exportImage}
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 my-1 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <DownloadIcon
+                    className="w-5 h-5 mr-2 -ml-1"
+                    aria-hidden="true"
+                  />
+                  Download Image
                 </button>
               </span>
             </div>
